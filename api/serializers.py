988b44cbe5fd4258ccda4b88868ae875594aab0d
pratik <pdhakal906@gmail.com>
2025-01-11 11:10:53 +0545

    delete frontend and backend

--- api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import News
-
-
-class NewsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = News
-        fields = ("id", "title", "link", "image_url", "source", "created_at")
