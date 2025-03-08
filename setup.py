afcedb7a698b8a21dfe5f7c3ae183c52867cebea
prati1kdhaka1 <pratik@inspiringlab.com.np>
2025-03-08 20:13:44 +0545

    prep for deploy

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="newscraper",
     version="0.1",
     packages=find_packages(),
+     entry_points={"scrapy": ["settings = newscraper.settings"]},
     install_requires=[
         "scrapy"  # Add required dependencies
         "psycopg2-binary",
